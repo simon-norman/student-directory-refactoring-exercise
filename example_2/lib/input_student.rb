35723aa554d0151ce7c6dce08efc13410be7ffe0
Simon <01secn@gmail.com>
2019-02-13 12:33:47 +0000

    Refactored input student class, breaking up methods

--- example_2/lib/input_student.rb
+++ example_2/lib/input_student.rb
@@ -3,21 +3,19 @@ class InputStudent
     @student = student
   end
 
-  def user_input
+  def input
     puts "Please enter the first name, last name, birthplace and cohort of each student.\n"
 
-    puts "Enter first name:"
-    first_name = STDIN.gets.gsub("\n", "")
-
-    puts "Enter surname:"
-    surname = STDIN.gets.gsub("\n", "")
-
-    puts "Enter birthplace:"
-    birthplace = STDIN.gets.gsub("\n", "")
-
-    puts "Enter cohort:"
-    cohort = STDIN.gets.gsub("\n", "")
+    first_name = input_field('first_name')
+    surname = input_field('surname')
+    birthplace = input_field('birthplace')
+    cohort = input_field('cohort')
 
     @student.new(first_name: first_name, surname: surname, birthplace: birthplace, cohort: cohort)
+  end
+
+  def input_field(field_name)
+    puts "Enter #{field_name}"
+    STDIN.gets.gsub("\n", "")
   end
 end
--- example_2/spec/unit/input_student_spec.rb
+++ example_2/spec/unit/input_student_spec.rb
@@ -1,32 +1,33 @@
 require './lib/input_student.rb'
 
 describe InputStudent do
+  let (:first_name) { 'Simon' }
+  let (:surname) { 'Norman' }
+  let (:birthplace) { 'Maidstone' }
+  let (:cohort) { 'December' }
+
+  let (:new_student_args) { {
+    first_name: first_name, 
+    surname: surname, 
+    cohort: cohort, 
+    birthplace: birthplace
+  } }
+
   before(:each) do
     @student_double = double(:student)
     @student_class_double = double(:Student, new: @student_double)
     @student_input = InputStudent.new(@student_class_double)
+
+    allow(STDIN).to receive(:gets).and_return(first_name, surname, birthplace, cohort)
   end
 
   it 'should get a new student from the user and create new student instance' do
-    first_name = 'Simon'
-    surname = 'Norman'
-    birthplace = 'Maidstone'
-    cohort = 'December'
-
-    allow(STDIN).to receive(:gets).and_return(first_name, surname, birthplace, cohort)
-    @student_input.user_input
+    @student_input.input
     
-    expect(@student_class_double).to have_received(:new).with({
-      first_name: first_name, 
-      surname: surname, 
-      cohort: cohort, 
-      birthplace: birthplace
-    })
+    expect(@student_class_double).to have_received(:new).with(new_student_args)
   end
 
   it 'should return the new student' do
-    allow(STDIN).to receive(:gets).and_return("")
-
-    expect(@student_input.user_input).to eq(@student_double)
+    expect(@student_input.input).to eq(@student_double)
   end
 end
